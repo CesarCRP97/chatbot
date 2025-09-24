f85d6cf70031d408830636d02ab1ad89393e75c2
CesarCRP97 <ccrp97@hotmail.es>
2025-09-24 20:19:14 +0200

    google genai connected

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key= api_key)
+
+content_generated = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+print(content_generated.text)
+
+usage = content_generated.usage_metadata
+print(f"Prompt tokens: {usage.prompt_token_count}")
+print(f"Response tokens: {usage.candidates_token_count}")
+
+
 def main():
     print("Hello from chatbot!")
 
