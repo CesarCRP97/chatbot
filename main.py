8017209f65a1ad40a76c4e0ed53d614f8a51256f
CesarCRP97 <ccrp97@hotmail.es>
2025-09-25 16:05:33 +0200

    add verbose argument

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key= api_key)
 
-content_generated = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-
-print(content_generated.text)
-
-usage = content_generated.usage_metadata
-print(f"Prompt tokens: {usage.prompt_token_count}")
-print(f"Response tokens: {usage.candidates_token_count}")
-
+def verbose_output(user_prompt, response):
+    print(f"\nUser prompt: {user_prompt}\n")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main():
-    print("Hello from chatbot!")
+    # Si no ha introducido un comando, imprime error y termina.
+    if len(sys.argv) < 2:
+        print("This script needs a prompt to be used")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key= api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+        )
+
+    
+    if '--verbose' in sys.argv[2:]:
+        verbose_output(prompt, response)
+    else:
+        print(response.text)
+        
+
+    
 
 
 if __name__ == "__main__":
