6dd055f4bf7d9091fa16111f0ba03025bb6a73c8
CesarCRP97 <ccrp97@hotmail.es>
2025-09-26 13:38:51 +0200

    add style changes

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,31 +5,30 @@ import os
 # Y SI NO, DIGA EL NOMBRE PROPIO DEL DIRECTORIO
 
 def get_files_info(working_directory, directory="."):
-    try:
-        # crea el path absoluto del join
-        full_path = os.path.abspath(os.path.join(working_directory, directory))
+    # crea el path absoluto del join
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
 
-        # si no empieza con la direccion de working_directory 
-        # -> directory accede fuera del working_directory
-        if not full_path.startswith(os.path.abspath(working_directory)):
-            return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        
-        elif not os.path.isdir(full_path):
-            return f'   Error: "{directory}" is not a directory'
-        
-        
+    # si no empieza con la direccion de working_directory 
+    # -> directory accede fuera del working_directory
+    if not full_path.startswith(os.path.abspath(working_directory)):
+        return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    elif not os.path.isdir(full_path):
+        return f'   Error: "{directory}" is not a directory'
+    
+    
+    try:
         lines = []
         for c in sorted(os.listdir(full_path)):
             content_full_path = os.path.join(full_path, c)
-            
             name = c
             is_dir = os.path.isdir(content_full_path)
             file_size = os.path.getsize(content_full_path)
-
-            new_line = f" - {name}: file_size={file_size} bytes, is_dir={is_dir}"
-            lines.append(new_line)
+            lines.append(
+                f" - {name}: file_size={file_size} bytes, is_dir={is_dir}"
+                )
 
         return '\n'.join(lines)
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error listing files: {e}"
     
