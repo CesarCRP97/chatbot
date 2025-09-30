721c25da9e53b9a3662fd3d04b356f50ab7cf898
CesarCRP97 <ccrp97@hotmail.es>
2025-09-30 17:39:29 +0200

    add schema for running a python file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import run
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
@@ -41,3 +42,20 @@ def run_python_file(working_directory, file_path, args=[]):
         return f"Error: executing Python file: {e}"
 
     
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs the python file specified by file_path with the args passed, constrained to the working directory. If the file doesn't exist or isn't a .py file then it raises an error.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file_path of the python file to run, relative to the working directory. If not provided, it returns an error message.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="The arguments needed to be passed to the python file.",
+            ),
+        },
+    ),
+)
